0492469b4eedd68816938750321a2a634e592177
Dariusz Tanajewski <dtandev@gmail.com>
2021-05-18 18:21:00 +0200

    Add files via upload
    ex Bank

--- Lab4/BankEx4/src/BankEx4.java
+++ Lab4/BankEx4/src/BankEx4.java
@@ -1,86 +1,189 @@
-class Bank {
+import org.w3c.dom.ls.LSOutput;
 
-    void income(){};
-    void transfer(){};
-    void interestChange(){};
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public Account[] bankAccounts = new Account[3];
-
+interface OperacjaBankowa {
+    public void wykonaj();
 }
 
-class Account {
-    long Balance;
-    Interest interest = new Interest();
+class SystemBankowy {
+    ArrayList <OperacjaBankowa> operacjeBankowa;
 
-    void doOperation(Operation operation){
-        operation.execute();
-    };
-
-}
-
-class Operation {
-    void execute() {};
-}
-
-class InterestChange extends Operation{
-    Account account;
-    Interest interest;
-
-    void execute() {};
-}
-
-class Income extends Operation{
-    Account account;
-    long incomeAmount;
-
-    public Income(int i) {
-        this.incomeAmount = i;
+    public SystemBankowy() {
+        operacjeBankowa = new ArrayList<OperacjaBankowa>();
     }
 
-    void execute(
-    ) {
-        System.out.print("Zwieksz saldo konta");
-    };
+    public void ustawOperacje(OperacjaBankowa operacjaBankowa) {
+        this.operacjeBankowa.add(operacjaBankowa);
+    }
+
+    public void wykonajOperacje() {
+        for (OperacjaBankowa i : operacjeBankowa) {
+            i.wykonaj();
+        }
+    }
+
 }
 
-class Transfer extends Operation{
+
+class Account {
+    double balance;
+    double debt;
+    ArrayList<Transfer> zleceniaStale;
+    boolean authorization;
+    String pin;
+
+    Account(String PIN) {
+        balance = 0.00;
+        debt = 0.00;
+        zleceniaStale = new ArrayList<Transfer>();
+        this.authorization = true;
+        this.pin = PIN;
+    }
+
+    public void withdrawMoney(double ammount) {
+        this.balance = this.balance - ammount;
+        System.out.println("Wypłacono: "+ ammount);
+    }
+
+    public void depositMoney(double ammount) {
+        this.balance = this.balance + ammount;
+        System.out.println("Wpłacono: "+ammount);
+    }
+
+    public void showBalance() {
+        System.out.println("Obecny stan konta: "+balance);
+    }
+
+    public void borrowMoney (double amount) {
+        this.debt = this.debt+amount;
+        this.balance = this.balance+amount;
+    }
+
+    public void returnMoney (double amount) {
+        this.debt = this.debt-amount;
+        this.balance = this.balance-amount;
+    }
+
+    public void dodajZlecenieStale(Transfer transfer) {
+        this.zleceniaStale.add(transfer);
+    }
+
+    public String getPin() {
+        return this.pin;
+    }
+
+}
+
+
+class Transfer implements OperacjaBankowa {
     Account from;
     Account to;
 
-    void execute() {};
+    double amount;
+
+    public Transfer(Account from, Account to, double amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+    public void wykonaj() {
+        if (from.authorization) {
+            Scanner pinReader = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("W celu autoryzacji wprowadź kod PIN:");
+            String PIN = pinReader.nextLine();  // Read user input
+
+            if (PIN.equals(from.getPin())) {
+                System.out.println("Rozpoczynam transfer "+amount+ " zł.");
+                from.withdrawMoney(amount);
+                to.depositMoney(amount);
+            } else {
+                System.out.println("Nieprawidłowy PIN. Operacja odrzucona.");
+            }
+        } else {
+            System.out.println("Rozpoczynam transfer "+amount+ " zł.");
+            from.withdrawMoney(amount);
+            to.depositMoney(amount);
+        }
+    }
+
 }
+class Wplata implements OperacjaBankowa{
+    Account to;
+    double amount;
 
-class Interest {
-    void compute(){};
-}
+    public Wplata(Account to, double amount) {
+        this.to = to;
+        this.amount = amount;
+    }
 
-class interestA extends Interest {}
-
-class interestB extends Interest {}
-
-class interestC extends Interest {}
-
-
-public class BankEx4 {
-
-    public static void main(String[] args) {
-        Bank IdeaBank = new Bank();
-        Bank SantanderBank = new Bank();
-
-        IdeaBank.bankAccounts = new Account[1];
-        SantanderBank.bankAccounts = new Account[1];
-
-        IdeaBank.bankAccounts[0] = new Account();
-        IdeaBank.bankAccounts[0].Balance = 1000;
-        IdeaBank.bankAccounts[0].interest = new interestA();
-
-        SantanderBank.bankAccounts[0] = new Account();
-        SantanderBank.bankAccounts[0].Balance = 2000;
-        SantanderBank.bankAccounts[0].interest = new interestB();
-
-        Operation o = new Income(2000);
-        IdeaBank.bankAccounts[0].doOperation(o);
-
+    public void wykonaj() {
+        to.depositMoney(amount);
     }
 }
 
+class Wyplata implements OperacjaBankowa {
+    Account from;
+    double amount;
+
+    public Wyplata(Account from, double amount) {
+        this.from = from;
+        this.amount = amount;
+    }
+
+    public void wykonaj() {
+        from.withdrawMoney(amount);
+    }
+}
+
+class SprawdzenieStanuKonta implements OperacjaBankowa {
+    Account account;
+
+    public SprawdzenieStanuKonta(Account account) {
+        this.account = account;
+    }
+
+    public void wykonaj() {
+        account.showBalance();
+    }
+}
+
+
+class UstawienieZlecen implements OperacjaBankowa {
+    Account account;
+
+    public void SprawdzenieStanuKonta(Account account) {
+        this.account = account;
+    }
+
+    public void wykonaj() {
+        account.showBalance();
+    }
+}
+
+public class BankEx4 {
+    public static void main(String[] args) {
+        SystemBankowy systemBankowy = new SystemBankowy();
+        Account kontoJanaKowalskiego = new Account("1234");
+        Account kontoAdamaNowaka = new Account("4321");
+        System.out.println("Salda początkowe");
+        System.out.println("Saldo Jana Kowalskiego: "+kontoJanaKowalskiego.balance);
+        System.out.println("Saldo Adama Nowaka: "+kontoAdamaNowaka.balance);
+        Wplata wplata01 = new Wplata(kontoJanaKowalskiego, 3000);
+        Wplata wplata02 = new Wplata(kontoAdamaNowaka, 5000);
+        systemBankowy.ustawOperacje(wplata01);
+        systemBankowy.ustawOperacje(wplata02);
+
+        Transfer transfer1 = new Transfer(kontoAdamaNowaka, kontoJanaKowalskiego, 1000);
+        systemBankowy.ustawOperacje(transfer1);
+
+        // Wykonanie wszystkich operacji
+        systemBankowy.wykonajOperacje();
+        System.out.println("Salda po spłacie zadłużenia");
+        System.out.println("Saldo Jana Kowalskiego: "+kontoJanaKowalskiego.balance);
+        System.out.println("Saldo Adama Nowaka: "+kontoAdamaNowaka.balance);
+    }
+
+}
